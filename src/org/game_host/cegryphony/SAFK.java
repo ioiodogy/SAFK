d9571e7168b9db8aaf9de0f21fbaeef6a0f2a12e
Evan <Evan@Evan-PC>
2013-04-20 22:53:45 -0400

    Changed config and aded ingame commands to change it

--- src/org/game_host/cegryphony/SAFK.java
+++ src/org/game_host/cegryphony/SAFK.java
@@ -10,7 +10,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.*;
 /**
  *
  * @author Cale
@@ -32,14 +32,31 @@ public class SAFK extends JavaPlugin {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
         if(cmd.getName().equalsIgnoreCase("afk")){ // If the player typed /afk then do the following...
             Player player = (Player) sender;
-            player.sendMessage(ChatColor.YELLOW + SAFK.this.getConfig().getString("text"));
-            player.teleport(new Location(Bukkit.getWorld("world"), SAFK.this.getConfig().getInt("location.X"), SAFK.this.getConfig().getInt("location.Y"), SAFK.this.getConfig().getInt("location.Z")));
+            player.sendMessage(ChatColor.YELLOW + SAFK.this.getConfig().getString("config.text"));
+            player.teleport(new Location(Bukkit.getWorld("world"), SAFK.this.getConfig().getInt("config.location.X"), SAFK.this.getConfig().getInt("config.location.Y"), SAFK.this.getConfig().getInt("config.location.Z")));
             return true;
         } //If this has happened the function will return true.
             // If this hasn't happened the a value of false will be returned.
+        else if(cmd.getName().equalsIgnoreCase("setafk")){
+            Player player = (Player) sender;
+            player.sendMessage(ChatColor.RED + "You have set the new AFK spot!");
+            Location loc = player.getLocation();
+            this.getConfig().set("config.location.X", loc.getBlockX());
+            this.getConfig().set("config.location.Y", loc.getBlockY());
+            this.getConfig().set("config.location.Z", loc.getBlockZ());
+            this.saveConfig();
+         return true;   
+        }
+        /*else if(cmd.getName().equalsIgnoreCase("afktimer")){
+            
+            
+        return true;
+        }*/
         return false;
+        }
+        
     }
-}
+
 /*
 permissions:
     afk.*:
