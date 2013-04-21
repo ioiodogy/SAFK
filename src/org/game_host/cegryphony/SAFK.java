4bbcb8d1dba32eb24c4a1126ff02bfc95b86b421
Evan <Evan@Evan-PC>
2013-04-20 21:36:38 -0400

    Added Config Functionality

--- src/org/game_host/cegryphony/SAFK.java
+++ src/org/game_host/cegryphony/SAFK.java
@@ -1,15 +1,16 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package org.game_host.cegryphony;
 
+import java.util.*;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.event.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.configuration.file.FileConfiguration;
 /**
  *
  * @author Cale
@@ -18,6 +19,7 @@ public class SAFK extends JavaPlugin {
     @Override
     public void onEnable(){
         getLogger().info("onEnable has been invoked!");
+        this.saveDefaultConfig();
     }
  
     @Override
@@ -30,8 +32,8 @@ public class SAFK extends JavaPlugin {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
         if(cmd.getName().equalsIgnoreCase("afk")){ // If the player typed /afk then do the following...
             Player player = (Player) sender;
-            //player.sendMessage(ChatColor.YELLOW + "You will now be telported to the AFK Area.
-            player.teleport(new Location(Bukkit.getWorld("world"), 208, 64, 151));
+            player.sendMessage(ChatColor.YELLOW + SAFK.this.getConfig().getString("text"));
+            player.teleport(new Location(Bukkit.getWorld("world"), SAFK.this.getConfig().getInt("location.X"), SAFK.this.getConfig().getInt("location.Y"), SAFK.this.getConfig().getInt("location.Z")));
             return true;
         } //If this has happened the function will return true.
             // If this hasn't happened the a value of false will be returned.
@@ -46,3 +48,4 @@ permissions:
 
 
 */
+ 
